e2be5bf3b6694e637a6efbf4fb043957128ef0ad
stalkcomrade <stanislavpoznyakof@gmail.com>
2019-07-14 17:46:29 +0200

    new .gitignore

---
+++ svelte-test.svelte
@@ -0,0 +1,28 @@
+<div class="figure" style="width: 600px;">
+  <div class="outer" on:mouseover="set({pres_n: undefined})">
+    <h1> TITLE STLK</h1>
+    </div>
+</div>
+
+
+<style>
+  
+</style>
+
+<script>
+
+  import LayerCake from 'layercake';
+        
+  function range(n){
+    return Array(n).fill().map((_, i) => i);
+  }
+  
+  export default {
+    data () {
+      return {
+        img: ""
+      };
+    },
+    helpers: {range}
+  };
+</script>
